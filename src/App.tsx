228e7554edda3a0b9b878e93f23127e4d9c45446
박하민 <haminjp@naver.com>
2022-05-18 02:05:33 +0900

    feat: add tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,54 @@
+import "@karrotframe/tabs/index.css";
+
+import { Tabs } from "@karrotframe/tabs";
+import { useState } from "react";
+
 const App = () => {
-  return <div>App</div>;
+  const [activeKey, setActiveKey] = useState<string>("44589");
+
+  return (
+    <Tabs
+      tabs={[
+        {
+          key: "44589",
+          buttonLabel: "프로젝트 소개",
+          render() {
+            return <div>프로젝트 소개</div>;
+          },
+        },
+        {
+          key: "63119",
+          buttonLabel: "자기소개",
+          render() {
+            return <div>자기소개</div>;
+          },
+        },
+        {
+          key: "46250",
+          buttonLabel: "지원동기",
+          render() {
+            return <div>지원동기</div>;
+          },
+        },
+        {
+          key: "21882",
+          buttonLabel: "기술스택",
+          render() {
+            return <div>기술스택</div>;
+          },
+        },
+        {
+          key: "64048",
+          buttonLabel: "경험",
+          render() {
+            return <div>경험</div>;
+          },
+        },
+      ]}
+      activeTabKey={activeKey}
+      onTabChange={(key) => setActiveKey(key)}
+    />
+  );
 };
 
 export default App;
