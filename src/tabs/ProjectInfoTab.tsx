95036c71fb02e5f8f24b0b1e7f107c29f8a546c5
박하민 <haminjp@naver.com>
2022-05-18 17:19:47 +0900

    change: update text

--- src/tabs/ProjectInfoTab.tsx
+++ src/tabs/ProjectInfoTab.tsx
@@ -25,16 +25,12 @@ const ProjectInfoTab = () => {
       <Image src={CarrotsImage} alt="carrots" />
       <h3>프로젝트 소개</h3>
       <Paragraph>
-        <b>Karrotframe tabs</b>를 이용하여 프로젝트를 만들어 보았습니다.
+        <b>Karrotframe navigator</b>와 <b>Karrotframe tabs</b>를 사용하여
+        프로젝트 만들었습니다.
       </Paragraph>
       <Paragraph>
-        <b>Karrotframe navigator</b>도 사용해보려 시도하였으나 잘 사용이 되지
-        않아서 tabs를 이용하게 되었습니다.
-      </Paragraph>
-      <Paragraph>
-        navigator를 사용해보면서 경험한 과정을 README에 간략하게 적었습니다.
-        시간이 부족해서 깊게 파고들지는 않았지만 더 깊게 파고든다면 재미있고
-        유익한 경험이 될 것 같습니다!
+        navigator를 사용하면서 경험한 내용을 Karrotframe Trouble Shooting 노션
+        페이지에 간략하게 적었습니다.
       </Paragraph>
       <Paragraph>
         모바일 웹뷰 개발에 karrotframe을 사용한다면 큰 도움이 될 것 같다고
